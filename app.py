8fd831811e1c3ce2c595419e3adc8a5dcda1de85
“sherwin7” <sherwin@karunya.edu.in>
2019-12-08 16:06:19 +0530

    duagfu

--- app.py
+++ app.py
@@ -9,12 +9,34 @@ from keras import backend as K
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     K.clear_session()
     category = None
+    if request.method == 'POST':
+        if request.form['question']:
+
+                json_file = open('DEEP_MODEL.json', 'r')
+                loaded_model_json = json_file.read()
+                json_file.close()
+                loaded_model = model_from_json(loaded_model_json)
+                loaded_model.load_weights("DEEP_MODEL.h5")
+                with open('tokenizer.pickle', 'rb') as file:
+                    tokenizes= pickle.load(file)
+                file.close()
+
+                ques = str(request.form['question'])
+                ques = [ques]
+                seq = tokenizes.texts_to_sequences(ques)
+                padded = pad_sequences(seq, maxlen=200)
+                pred = loaded_model.predict(padded)
+                labels= ['unknown', 'what', 'when', 'who', 'affirmation']
+                category = labels[np.argmax(pred)]
+
+        else:
+            category = "type something"
     return render_template('home.html', category=category)
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True,port="5002")
